af93763234c85031c39cd392ab5ea6ffc170a185
jjems <jameslunox@gmail.com>
2025-07-21 17:54:48 +0900

    feat 좋아요 토글 상태 업데이트

--- src/main/java/com/trip_gg/controller/LikeController.java
+++ src/main/java/com/trip_gg/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.trip_gg.jwt.JwtTokenProvider;
 import com.trip_gg.service.LikeService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Request;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,9 +31,18 @@ public class LikeController {
         return ResponseEntity.ok("ok");
     }
 
-    @GetMapping("/likes/{posts_id}/count")
+    @GetMapping("/{posts_id}/count")
     public ResponseEntity<Integer> getLikeCount(@PathVariable int posts_id) {
         int like_count = likeService.getLike_count(posts_id);
         return ResponseEntity.ok(like_count);
+    }
+
+    @GetMapping("/{posts_id}/status")
+    public ResponseEntity<Boolean> isLiked(@PathVariable int posts_id, HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        String users_id = jwtTokenProvider.getUserIdFromToken(token);
+
+        boolean liked = likeService.isLiked(posts_id, users_id);
+        return ResponseEntity.ok(liked);
     }
 }
